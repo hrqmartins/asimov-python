829e5491362d41fac18e73803e5d3c13cbd3f67b
hrqmartins <hrqmartins129@gmail.com>
2025-07-12 23:47:35 -0300

    Interagindo com o usuário - varíaveis e tipos de dados.

---
+++ asimov.py
@@ -0,0 +1,5 @@
+print('Seja bem vindo ao ChatBot da NEXT!\n')
+
+nome_usuario = input('Antes eu gostaria de saber qual é o seu nome ? ')
+duvida = input(f'Olá {nome_usuario}, qual é a sua dúvida? ')
+print(f'A sua dúvida é {duvida}.')
