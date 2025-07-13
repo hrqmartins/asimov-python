628be4d8ef6a88072ad084fd1f0164a0490d13fd
hrqmartins <hrqmartins129@gmail.com>
2025-07-13 00:33:57 -0300

    Armazenando histórico de "conversa" em uma lista vazia.

--- asimov.py
+++
@@ -1,9 +0,0 @@
-print('Bem-vindo ao Nydra! (Aperte X para sair)\n')
-pergunta = input('Digite a sua pergunta: ')
-
-while True:
-    print('Ryu: aqui colocaremos a reposta do bot')
-    pergunta = input('Usuario: ')    
-    if pergunta.upper() == 'X':
-        break
-
--- nydra.py
+++ nydra.py
@@ -1,9 +1,12 @@
+mensagens = []
+
 print('Bem-vindo ao Nydra! (Aperte X para sair)\n')
-pergunta = input('Digite a sua pergunta: ')
 
 while True:
-    print('Ryu: aqui colocaremos a reposta do bot')
-    pergunta = input('Usuario: ')    
-    if pergunta.upper() == 'X':
+    pergunta = input('Qual é a sua pergunta? ')
+    print('Bot: ')
+    mensagens.append(pergunta)
+    if pergunta == 'x':
+        print('Voce escolheu sair!')
+        print(f'Mensagens que vc enviou ate agora foram: \n {mensagens}')
         break
-
