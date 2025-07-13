a4bc71d1eba0e77e4dd184b5e5f07bbd9cf3869c
hrqmartins <hrqmartins129@gmail.com>
2025-07-13 00:16:37 -0300

    Atualização do nome do arquivo principal.

---
+++ nydra.py
@@ -0,0 +1,9 @@
+print('Bem-vindo ao Nydra! (Aperte X para sair)\n')
+pergunta = input('Digite a sua pergunta: ')
+
+while True:
+    print('Ryu: aqui colocaremos a reposta do bot')
+    pergunta = input('Usuario: ')    
+    if pergunta.upper() == 'X':
+        break
+
